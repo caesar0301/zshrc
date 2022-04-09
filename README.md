97bb6d5a1d04f0286c73a21f6325ab2b0c7c115d
xiaming.cxm <xiaming.cxm@alibaba-inc.com>
2022-04-09 16:53:22 +0800

    Update

---
+++ README.md
@@ -0,0 +1,7 @@
+# zsh.d
+
+## Install
+
+```
+git clone --recursive https://github.com/caesar0301/zsh.d.git ~/.zsh.d
+```
--- install.sh
+++ install.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 ZSH_HOME=${HOME}/.zsh.d
 
-if [ ! -e $ZSH_HOME/bin ]; then
-    mkdir -p $ZSH_HOME/bin
-    cp .zsh.d/bin/* $ZSH_HOME/bin
-fi
-
-echo "Download zplug"
-if [ ! -e $ZSH_HOME/zplug ]; then
-    git clone https://github.com/zplug/zplug.git $ZSH_HOME/zplug
-else
-    echo "$ZSH_HOME/zplug existed, skip"
-fi
+cd $ZSH_HOME
 
 echo "Download on-my-zsh"
 if [ ! -e $HOME/.oh-my-zsh ]; then
@@ -21,21 +11,9 @@ else
 fi
 
 if [ ! -e $HOME/.zshrc ]; then
-    cp .zshrc $HOME/.zshrc
+    cp default.zshrc $HOME/.zshrc
 else
     echo "$HOME/.zshrc existed, skip"
-fi
-
-if [ ! -e $ZSH_HOME/zplug_loader.sh ]; then
-    cp .zsh.d/zplug_loader.sh $ZSH_HOME
-else
-    echo "$ZSH_HOME/zplug_loader.sh existed, skip"
-fi
-
-if [ ! -e $ZSH_HOME/my_profile ]; then
-    cp .zsh.d/my_profile $ZSH_HOME
-else
-    echo "$ZSH_HOME/my_profile existed, skip"
 fi
 
 echo """Change the default shell as zsh:
