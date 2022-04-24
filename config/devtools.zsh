1cc8f5f7163fa208bf2be7bea2875c97d57fc5f0
xiaming.cxm <xiaming.cxm@alibaba-inc.com>
2022-04-24 16:12:36 +0800

    update jenv

--- config/devtools.zsh
+++ config/devtools.zsh
@@ -22,29 +22,6 @@ fi
 
 # jenv
 if [ -e "$HOME/.jenv" ]; then
-    eval export PATH="${HOME}/.jenv/shims:${PATH}"
-    export JENV_SHELL=zsh
-    export JENV_LOADED=1
-    unset JAVA_HOME
-    export JENV_ROOT=$HOME/.jenv
-    export PATH=$JENV_ROOT/bin:$PATH
+    export PATH="$HOME/.jenv/bin:$PATH"
     eval "$(jenv init -)"
-    eval "$(jenv enable-plugin export)"
-    jenv rehash 2>/dev/null
-    jenv() {
-        typeset command
-        command="$1"
-        if [ "$#" -gt 0 ]; then
-            shift
-        fi
-
-        case "$command" in
-        enable-plugin | rehash | shell | shell-options)
-            eval $(jenv "sh-$command" "$@")
-            ;;
-        *)
-            command jenv "$command" "$@"
-            ;;
-        esac
-    }
 fi
